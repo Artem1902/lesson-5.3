4b226a1a5f508c92f93e5ffd412f0d24a9249367
Artem Kolhanov <artem.kolganov11@gmail.com>
2025-01-29 22:02:49 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Task 3</h1>
+  <products-cards-list></products-cards-list>
+</template>
+
+<script>
+
+
+import ProductsCardsList
+  from "@/components/Products/ProductsCardsList.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ProductsCardsList
+  }
+}
+</script>
+
+<style>
+
+</style>
